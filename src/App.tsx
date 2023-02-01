a2b73eb35d9bbf653cf2ec7da294025fc0d6c57f
Rivolta, Romain <romain.rivolta@ge.com>
2023-02-01 09:37:29 +0100

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Menu from './components/Menu';
 import NotFound from './components/NotFound';
+import Recipes from './components/Recipes';
 
 const App = () => {
   return (
@@ -10,7 +11,7 @@ const App = () => {
       <Menu />
       <Routes>
         <Route path='/' />
-        <Route path='/recipes' />
+        <Route path='/recipes' element={<Recipes />} />
         <Route path='/mytraining' />
         <Route path='/fitness' />
         <Route path='/caloriecalculator' />
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import "./Recipes.scss";
 
 const Recipes = () => {
-    const {t} = useTranslation('recipes');
-    return(
-        <div id="sidebar">
-            <h3>{t('quick access')}</h3>
-            <div className="checklist quick-access">
-                <ul>
-                </ul>
+    const { t } = useTranslation(['recipes','']);
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="recipes-grid order-lg-2 col-xl-9 col-lg-8">
+                    <header>
+                        <div className="me-3 mb-3">
+                            {(t('showing'))}
+                            <strong>1-12</strong>
+                            {(t('of'))}
+                            <strong>158</strong>
+                            {(t('recipes'))}
+                        </div>
+                        <div className="me-3 mb-3">
+
+                        </div>
+                        <div className="mb-3 d-flex align-items-center">
+
+                        </div>
+                    </header>
+                    <div className="row">
+
+                    </div>
+                </div>
+                <div className="sidebar col-xl-3 order-lg-1">
+                    <div className="sidebar-block px-3 px-lg-0 me-lg-4">
+                        <a className="d-lg-none block-toggler" data-bs-toggle="collapse" data-bs-target="filter-recipes" aria-expanded="true">
+                            {t('filter by meal')}
+                        </a>
+                        <div className="collapse show" id="filter-recipes">
+                            <h6 className="sidebar-heading d-none d-lg-block">{t('meal')}</h6>
+                            <form>
+                                
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     )
 }
 
